ce44b3cd99d63396cbbed8612a18922781bc79f3
Panagiotis Siatras <azazeal@users.noreply.github.com>
2022-10-01 17:29:29 +0300

    go: removed test dependencies

--- pause_test.go
+++ pause_test.go
@@ -5,8 +5,6 @@ import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
 )
 
 const forever = time.Duration(math.MaxInt64)
@@ -23,7 +21,7 @@ func TestCanceledContext(t *testing.T) {
 func TestTimedOutContext(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	ensure(ctx, t, forever, time.Second<<1)
@@ -32,7 +30,7 @@ func TestTimedOutContext(t *testing.T) {
 func TestContextCancelation(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := context.WithCancel(context.TODO())
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	_ = time.AfterFunc(time.Second, cancel)
@@ -40,11 +38,15 @@ func TestContextCancelation(t *testing.T) {
 	ensure(ctx, t, forever, time.Second<<1)
 }
 
+// ensure ensures that the amount of time For(ctx, timeout) blocks for is within [-delta, delta].
 func ensure(ctx context.Context, t *testing.T, timeout, delta time.Duration) {
 	t.Helper()
 
-	at := time.Now()
+	startedAt := time.Now()
 	For(ctx, timeout)
+	stoppedAt := time.Now()
 
-	require.WithinDuration(t, at, time.Now(), delta)
+	if dt := startedAt.Sub(stoppedAt); dt < -delta || dt > delta {
+		t.Fatalf("max delta between %v and %v allowed is %v, but was %v", startedAt, stoppedAt, delta, dt)
+	}
 }
